4eb2796feb192035a17c296f68e404d7e3edc95c
linux32532 <94577827+linux32532@users.noreply.github.com>
2021-12-10 21:41:17 -0800

    Add files via upload

---
+++ typingbot.py.py
@@ -0,0 +1,109 @@
+from datetime import time
+
+from pygments.lexer import words
+from pywin.tools import browser
+from selenium import webdriver
+
+url = "http://monkeytype.com/"
+with webdriver.Chrome() as driver:
+    driver.get(url)
+
+
+def Controller():
+    pass
+
+
+keyboard = Controller()
+
+
+def new_func4(path: object) -> object:
+    driver = webdriver.Chrome(path)
+    return driver
+
+
+driver = new_func4()
+driver.get('https://monkeytype.com/')
+
+
+def controller():
+    pass
+
+
+def given_word_count():
+    # time.sleep() allows the user some time to select the desired test mode on monkeytype.com
+    new_var = time.sleep(6)
+    new_var
+    new_func()
+
+
+def new_func():
+    new_func1()
+
+
+def new_func1():
+    new_func2()
+
+
+def new_func2():
+    new_func3()
+
+
+def new_func3():
+    words = browser.find_elements_by_class_name('word')
+
+
+def type_text(words):
+    for word in words:
+        if "'" in word.get_attribute('textContent') or '"' in word.get_attribute('textContent'): word
+
+
+def new_func5():
+    pass
+
+
+new_func5()
+type_text()
+
+new_func4()
+webdriver.get('https://monkeytype.com/')
+# selenium
+# webdriver.Chrome(path)
+# webdriver
+# driver.get('https://monkeytype.com/')
+MY_CHROME_DRIVER_PATH = 'C:\Program Files\chromedriver.exe'
+print('you type really slow a computer is faster then you(:')
+
+
+def new_func1(words):
+    type_text(words)
+
+
+def new_func1(words):
+    new_var = new_func1(words)
+    return new_var
+
+
+def new_func1(words):
+    new_var = new_func1(words)
+    return new_var
+
+
+def new_func4():
+    controller()
+
+
+def new_func4():
+    new_var = new_func1(words)
+
+
+def new_func4() -> object:
+    new_func4()
+
+
+def new_func5():
+    new_func4()
+
+
+def new_func5():
+    new_func5(type_text)
+
